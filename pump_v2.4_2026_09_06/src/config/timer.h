5309cbdfd5e9c3c93d63a141aecb23acb59db4fb
Тимур Кузнецов <zelezo.360@gmail.com>
2026-09-06 11:28:22 +0900

    Update timer.h
    исправил тип констант

--- pump_v2.4_2026_09_06/src/config/timer.h
+++ pump_v2.4_2026_09_06/src/config/timer.h
@@ -5,9 +5,9 @@
 // таймер для насоса
 namespace Timer_nasos
 {
-    // constexpr int CHEK_PIN = 21;
-    constexpr uint32_t MIN_TO_MS = 60 * 1000U;                         // число для конвертации минут в мс
-    constexpr uint32_t SET_DURATION_MINUTES = 15;                      // длительность таймера в минутах
+    
+    constexpr uint16_t MIN_TO_MS = 60 * 1000U;                         // число для конвертации минут в мс
+    constexpr uint8_t SET_DURATION_MINUTES = 15;                      // длительность таймера в минутах
     constexpr uint32_t DURATION_MS = SET_DURATION_MINUTES * MIN_TO_MS; // итоговая длительность в мс. Для изменеия времени измените SET_DURATION_MINUTES
     unsigned long pumpStartTime = 0;                                   // хранилище времени для отсчета до BOUND_MINUTES
     bool stateTimer;
